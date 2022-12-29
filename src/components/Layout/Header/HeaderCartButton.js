e56d12ca0bc4e515e4bde76b133a3d2aec15db92
David Perelman <dperelman3@gmail.com>
2022-12-29 11:15:42 +0200

    update HeaderCartButton.js component

--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import CartIcon from '../../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = () => {
   return (
     <button className={classes.button}>
-      Cart <span className={classes.badge}>0</span>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
     </button>
   );
 };
