497062028e3b2d9a0b5045fdfb5e6ad3c6e5c494
David Perelman <dperelman3@gmail.com>
2022-12-29 11:55:53 +0200

    update MealItem.js component

--- src/components/Meals/AvailableMeals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals/AvailableMeals.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Card from '../../UI/Card/Card';
 import MealItem from '../MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -2,16 +2,17 @@ import React from 'react';
 import classes from './MealItem.module.css';
 
 const MealItem = ({ meal }) => {
-  console.log(meal);
+  const price = `$${meal.price.toFixed(2)}`;
+
   return (
-    <div className={classes.meal}>
+    <li className={classes.meal}>
       <div className={classes.mealItem}>
         <h3>{meal.name}</h3>
         <span className={classes.description}>{meal.description}</span>
-        <span className={classes.price}>{meal.price}</span>
+        <span className={classes.price}>{price}</span>
       </div>
       <div>MealItem2</div>
-    </div>
+    </li>
   );
 };
 
