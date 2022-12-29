b4ef31012e8fb6ad72d86e7b77da07c9513f1a38
David Perelman <dperelman3@gmail.com>
2022-12-29 11:09:36 +0200

    setup HeaderCartButton.js component

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,16 +1,14 @@
 import React, { Fragment } from 'react';
 import classes from './Header.module.css';
-import btnClasses from './HeaderCartButton.module.css';
 import image from '../../../assets/meals.jpg';
+import HeaderCartButton from '../../Cart/HeaderCartButton';
 
 const Header = () => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button className={btnClasses.button}>
-          Cart <span className={classes.badge}>0</span>
-        </button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={image} alt='A table full of delicious food!' />
